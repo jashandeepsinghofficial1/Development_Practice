92bdb79e60bae33d17472f669aab872dd160af67
Jashandeep Singh <jashandeepsinghofficial1@gmail.com>
2025-07-16 19:18:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-while (true) {
-  while (true) {
-    while (true) {
-      while (true) {
+while (false) {
+  while (false) {
+    while (false) {
+      while (false) {
         console.log(78*78*78*78*78*7*87*87*87*87*87*87*78*78*78*8*87*78*78*78*78*787*87*87*87*8*8*8*8*8*8*8*8*84*8*84*84*84*84*84*81*81*874*84*84*8*48*4821*48*48*482*48*248*284*82*482*482*482*84*284*82*482*482*482*482*84,`A week ago a friend invited a couple of other couples over for dinner. Eventually, the food (but not the wine) was cleared off the table for what turned out to be some fierce Scrabbling. Heeding the strategy of going for the shorter, more valuable word over the longer cheaper word, our final play was “Bon,” whichas luck would have it!happens to be a Japanese Buddhist festival, and not, as I had originally asserted while laying the tiles on the board, one half of a chocolate-covered cherry treat. Anyway, the strategy worked. My team only lost by 53 points instead of 58.
 
 Just the day before, our host had written of the challenges of writing short. In journalismmy friends chosen trade, and mostly my own, tooMark Twains observation undoubtedly applies: “I didnt have time to write a short letter, so I wrote a long one instead.” The principle holds across genres, in letters, reporting, and other writing. Its harder to be concise than to blather. (Full disclosure, this blog post will clock in at a blather-esque 803 words.) Good writing is boiled down, not baked full of air like a souffl??. No matter how yummy souffl??s may be. Which they are. Yummy like a Grisham novel.
